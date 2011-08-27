9a9a09f70779d546a5e50215b308ff016c6684b5
knoodake <knoodrake@gmail.com>
2011-08-27 22:29:36 +0200

    Change-Id: I957cd7afb45daa50c3f82360b119c2dd01e70057

--- knoocraft/src/knoodrake/knoocraft/R.java
+++ knoocraft/src/knoodrake/knoocraft/R.java
@@ -1,7 +1,14 @@
 package knoodrake.knoocraft;
 
 import java.util.ListResourceBundle;
-
+/**
+ * @author melendil, knoodrake
+ * 
+ * @TODO Y'a un pb de taille avec cette classe, c'est que quand, gentillement on demande un truc à la con genre
+ * une chaine de caractère mais qu'elle existe pas, ca fait tout plantouyer, exception non catché et tout.
+ * Faudrait qu'il renvoi rien, ou un message, ou la clef, ou je sais pas, mais bon, une exception non gérée pour une pauvre
+ * chaine manquante c'est un peu Hard je trouve.
+ */
 public class R {
 
 	public static String get(String key) {
